7090abd48ec9a6601feed0838d799d7c3410ba55
nichia <nichia@gmail.com>
2019-11-11 16:52:35 -0500

    TechJobs - OO : Display a single job and create new jobs

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,10 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
+import org.launchcode.models.Employer;
+import org.launchcode.models.Location;
+import org.launchcode.models.CoreCompetency;
+import org.launchcode.models.PositionType;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -24,7 +29,10 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        Job theJob;
 
+        theJob = jobData.findById(id);
+        model.addAttribute("job", theJob);
         return "job-detail";
     }
 
@@ -41,7 +49,20 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+            return "new-job";
+        }
 
+        String name = jobForm.getName();
+        Employer emp = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location loc = jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType posType = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+        CoreCompetency coreComp = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+
+        Job newJob = new Job(name, emp, loc, posType, coreComp);
+
+        jobData.add(newJob);
+
+        return ("redirect:" +  "?id=" + newJob.getId());
     }
 }
